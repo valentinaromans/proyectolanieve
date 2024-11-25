c0306068f84a2cedcf3bd662582ccb3657b613fc
jonathancerda123 <jonathanygnaciocerdagarrido@gmail.com>
2024-11-25 08:05:51 -0300

    requirements creado, vacio

--- lanieve/settings.py
+++ lanieve/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
 
