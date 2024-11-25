646e595b19039cd161245155f59ca6f5070d9d72
jonathancerda123 <jonathanygnaciocerdagarrido@gmail.com>
2024-11-25 18:15:01 -0300

    hola mundo

--- lanieve/settings.py
+++ lanieve/settings.py
@@ -128,8 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Dire ctorio donde colocaremos archivos estáticos globales.
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
 
 
 MEDIA_URL = '/media/'
