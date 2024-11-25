5908439c5608511d5252cdd09354c2469149d7af
jonathancerda123 <jonathanygnaciocerdagarrido@gmail.com>
2024-11-25 17:58:09 -0300

    a1

--- lanieve/urls.py
+++ lanieve/urls.py
@@ -27,6 +27,5 @@ urlpatterns = [
     path('pedidos/', include('pedidos.urls', namespace='pedidos')),
 ]
 
-
-if settings.DEBUG: 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
